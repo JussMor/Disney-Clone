5e2e9ce507ef4079487ac886b11402ef9eba62f2
Junior Moreira <jussmor@outlook.es>
2022-01-09 17:38:24 -0500

    nominal

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ This is the ReactJS Disney Plus Clone.
 
 Build it today!
 PREREQUISITES:
-Sign up for a Firebase account HERE
-Install Node JS in your computer HERE
-Download all the images and videos HERE
+Sign up for a Firebase account 
+Install Node JS in your computer 
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -15,7 +15,7 @@ const Login = (props) => {
         <Content>
           <CTA>
             <CTALogoOne src="/assets/images/cta-logo-one.svg" alt="" />
-            <SignUp>GET ALL THERE</SignUp>
+            <SignUp>INICIA SESION PORFAVOR</SignUp>
             <Description>
               Get Premier Access to Raya and the Last Dragon for an additional fee
               with a Disney+ subscription. As of 03/26/21, the price of Disney+
